800b00e5e691ff19ab693234458a19d3185e1aa8
khoivan88 <33493502+khoivan88@users.noreply.github.com>
2021-05-09 22:58:54 -0400

    chore: fix code comments

--- src/scrape-ncf-images.py
+++ src/scrape-ncf-images.py
@@ -46,7 +46,7 @@ from scrapy.pipelines.files import FilesPipeline
 class MyFilesPipeline(FilesPipeline):
 
     def file_path(self, request, response=None, info=None, *, item=None):
-        # return item['image_brand'] + os.path.basename(urlparse(request.url).path)
+        # Save to 'brand' folder with the sku (lower case) as filename
         return f"{item['image_brand']}/{item['image_name']}.png"
 
 
