f4bb02709e501ad4a16000be3f23909b874e707e
Khoi Van <33493502+khoivan88@users.noreply.github.com>
2021-05-26 19:29:16 -0400

    feat: add scraping result from skytechfireplaceremotes.com

--- src/scrape_skytechfireplaceremotes_images.py
+++ src/scrape_skytechfireplaceremotes_images.py
@@ -192,7 +192,7 @@ if __name__ == '__main__':
         # 'CSV_EXPORT_FILE': THIS_SPIDER_RESULT_FILE,
         'MEDIA_ALLOW_REDIRECTS': True,
         'FILES_STORE': str(DOWNLOAD_FOLDER),
-        # 'MYFILESPIPELINE_FILES_EXPIRES': 0,
+        'FILES_EXPIRES': 0,
         'ITEM_PIPELINES': {
             '__main__.MyFilesPipeline': 1,
         },
@@ -222,12 +222,6 @@ if __name__ == '__main__':
 
     process = CrawlerProcess(settings=settings)
     process.crawl(SkytechfireplaceremotesSpider)
-    process.start()
-    # with console.status("[bold green]Scraping images...") as status:
-    #     process.start()
-
-    # Disable for now!!
-    # transform_images()
-
-    # image_files = {f.resolve() for f in Path(DOWNLOAD_FOLDER).glob('**/*.*')}
-    # logging.info(f'{len(image_files)=}')
+    # process.start()
+    with console.status("[bold green]Scraping images from skytechFireplaceRemotes.com ...") as status:
+        process.start()
